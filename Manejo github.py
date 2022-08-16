a334d7039a115e97533cb6eea01b7c9805b4ea01
Nelson David Naranjo Alvarez <nelsondavid2080@hotmail.com>
2022-08-16 18:19:49 -0500

    Ingreso de direccion del usuario

--- Manejo github.py
+++ Manejo github.py
@@ -1,8 +1,6 @@
 print("Manejo Gitgub")
 
-<<<<<<< HEAD
 
 edad = 25
-=======
 a = "Nelson David Naranjo Alvarez"
->>>>>>> b46fb513b8df69abb68ea59e14c54146c913b544
+direccion = "Avenida 45b diagonal 67c-10"
